d52a819ab2a1f387c01d3b1c3a941fd752c5c54c
LukasHolm <lukasholm@hotmail.com>
2020-09-22 09:43:40 +0000

    auto-format

--- main.py
+++ main.py
@@ -2,35 +2,37 @@ import math
 import re
 
 
-
-
 def find_d(coefficients):
-	a = coefficients[0]
-	b = coefficients[1]
-	c = coefficients[2]
-	d = b**2 - 4 * a * c
-	if d < 0:
-		print("the discriminant is less than zero, therefore there are no solution")
-	elif d == 0:
-		print("the discriminant equals zero, therefore there is one solution")
-	else:
-		print("the discriminant is above zero, therefore there are two solutions")
-		find_x(a, b, d)
-	
+    a = coefficients[0]
+    b = coefficients[1]
+    c = coefficients[2]
+    d = b**2 - 4 * a * c
+    if d < 0:
+        print(
+            "the discriminant is less than zero, therefore there are no solution"
+        )
+    elif d == 0:
+        print("the discriminant equals zero, therefore there is one solution")
+    else:
+        print(
+            "the discriminant is above zero, therefore there are two solutions"
+        )
+        find_x(a, b, d)
 
-	
-def find_x(a, b ,d):
-	x1 = (-b + math.sqrt(d)) / (2*a)
-	x2 = (-b - math.sqrt(d)) / (2*a)
-	print("x1 = " + str(round(x1)))
-	print("x2 = " + str(round(x2)))
+
+def find_x(a, b, d):
+    x1 = (-b + math.sqrt(d)) / (2 * a)
+    x2 = (-b - math.sqrt(d)) / (2 * a)
+    print("x1 = " + str(round(x1)))
+    print("x2 = " + str(round(x2)))
+
 
 def run():
-	global poly
-	poly = input("please enter a polynomium: ")
-	COEFFICIENT_PATTERN = re.compile(r"(?<!x\^)(?<!x\^-)-?\d+")
-	coefficients = [int(c) for c in COEFFICIENT_PATTERN.findall(poly)]
-	find_d(coefficients)
+    global poly
+    poly = input("please enter a polynomial: ")
+    COEFFICIENT_PATTERN = re.compile(r"(?<!x\^)(?<!x\^-)-?\d+")
+    coefficients = [int(c) for c in COEFFICIENT_PATTERN.findall(poly)]
+    find_d(coefficients)
 
 
 run()

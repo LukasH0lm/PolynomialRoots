d8d56df1a203f79589e36a9704555130d1cd3826
LukasHolm <lukasholm@hotmail.com>
2020-09-23 11:00:07 +0000

    small QOL changes

--- main.py
+++ main.py
@@ -8,24 +8,27 @@ def find_d(coefficients):
     c = coefficients[2]
     d = b**2 - 4 * a * c
     if d < 0:
-        print("the discriminant is less than zero, therefore there are no solution")
+        print("\nThe discriminant is less than zero, therefore there are no solution")
     elif d == 0:
-        print("the discriminant is zero, therefore there is one solution")
+        print("\nThe discriminant is zero, therefore there is one solution")
         find_x_d0(a, b)
     else:
-        print("the discriminant is above zero, therefore there are two solutions")
+        print("\nThe discriminant is above zero, therefore there are two solutions")
         find_x(a, b, d)
 
 
 def find_x(a, b, d):
     x1 = (-b + math.sqrt(d)) / (2 * a)
     x2 = (-b - math.sqrt(d)) / (2 * a)
+    print("\nThe roots of " + poly + " are:")
     print("x1 = " + str(round(x1)))
     print("x2 = " + str(round(x2)))
 
+
 def find_x_d0(a, b):
-	x = -b / (2*a)
-	print("x = " + str(round(x)))
+    x = -b / (2 * a)
+    print("\nThe root of " + poly + " is:")
+    print("x = " + str(round(x)))
 
 
 def run():
